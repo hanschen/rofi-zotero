6d3ad3ad15855cc038ff7d7b41b3dd5423d67bdd
Hans Chen <contact@hanschen.org>
2023-05-16 17:04:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+rofi-zotero
+===========
+
+A simple script that uses [Rofi] to search for and open PDF/DjVu attachments
+from [Zotero].
+
+The script requires Python 3.7 or above (and Rofi, obviously).
+
+This repository was created for personal use, and bugs are to be expected.
+Make sure to keep backups of important files.
+
+
+[Rofi]: https://github.com/davatorium/rofi
+[Zotero]: https://www.zotero.org/
+
+
+Usage
+-----
+
+```bash
+rofi-zotero.py
+```
+
+Configuration
+-------------
+
+The configuration options are currently quite limited.
+
+You can change the format of the Rofi list using the `FORMAT` variables in
+`rofi-zotero.py`.
+
+
+Differences from other projects
+-------------------------------
+
+There are two similar projects with the same name:
+
+- [rolph-recto/rofi-zotero]
+- [MatthijsMars/rofi-zotero] (fork of the former)
+
+Here are some notable difference from the abovementioned projects:
+
+- The list of attachments is sorted.
+- It is also possible to open attached links
+  (for example, created using [ZotFile]).
+- It is easier to change the format of the list of attachment.
+- An error message is shown if an attachment is not found.
+
+
+[MatthijsMars/rofi-zotero]: https://github.com/MatthijsMars/rofi-zotero
+[rolph-recto/rofi-zotero]: https://github.com/rolph-recto/rofi-zotero
+[ZotFile]: http://zotfile.com/
+
+
+Credits
+-------
+
+Some parts of the code are based on [MatthijsMars/rofi-zotero] and the original
+project [rolph-recto/rofi-zotero].
---
+++ rofi-zotero.py
@@ -0,0 +1,314 @@
+#!/usr/bin/env python
+"""Use Rofi to search and open attachments from Zotero.
+
+Requirements:
+- Python 3.7 or newer
+- Rofi
+
+Author: Hans Chen (contact@hanschen.org)
+
+"""
+
+import os
+import shutil
+import sqlite3
+import subprocess
+import sys
+import tempfile
+
+from pathlib import Path
+
+
+DEFAULT_ZOTERO_PATH = Path.home() / "Zotero"
+DEFAULT_ZOTERO_BASE_DIR = Path.home() / "papers"
+DEFAULT_ROFI_EXTRA_FLAGS = [
+    "-i",           # case insensitive
+    "-p", "paper",  # prompt title
+]
+DEFAULT_VIEWER = "xdg-open %u"
+
+FORMAT_ITEM = "{author} ({year}) - {title}"
+FORMAT_ITEM_WITHOUT_YEAR = "{author} - {title}"
+FORMAT_ITEM_WITHOUT_AUTHOR = "Unknown ({year}) - {title}"
+FORMAT_ITEM_WITHOUT_AUTHOR_AND_YEAR = "Unknown - {title}"
+
+FORMAT_AUTHORS_SINGLE = "{author1}"
+FORMAT_AUTHORS_TWO = "{author1} and {author2}"
+FORMAT_AUTHORS_MULTIPLE = "{author1} et al."
+
+# Zotero directories and files
+_ZOTERO_STORAGE_DIR = "storage"
+_ZOTERO_SQLITE_FILE = "zotero.sqlite"
+
+# SQL queries for internal use
+_QUERY_AUTHORS = """
+    SELECT items.itemID,
+            creators.Lastname
+    FROM    ((creators
+              JOIN itemCreators
+                ON creators.creatorID = itemCreators.creatorID) AS t1
+             JOIN items
+               ON t1.itemID = items.itemID)
+"""
+
+_QUERY_TITLES = """
+    SELECT parentInfo.itemID,
+           parentItemDataValues.value,
+           attachmentInfo.key
+    FROM   itemAttachments
+           INNER JOIN items AS attachmentInfo
+                   ON attachmentInfo.itemID = itemAttachments.itemID
+           INNER JOIN itemData AS attachmentItemData
+                   ON attachmentItemData.itemID = attachmentInfo.itemID
+           INNER JOIN itemDataValues AS attachmentItemDataValues
+                   ON attachmentItemData.valueID =
+                      attachmentItemDataValues.valueID
+           INNER JOIN items AS parentInfo
+                   ON itemAttachments.parentItemID = parentInfo.itemID
+           INNER JOIN itemData AS parentItemData
+                   ON parentItemData.itemID = parentInfo.itemID
+           INNER JOIN itemDataValues AS parentItemDataValues
+                   ON parentItemDataValues.valueID = parentItemData.valueID
+    WHERE  attachmentItemData.fieldID = 1
+           AND parentItemData.fieldID = 1
+           AND ( itemAttachments.contentType LIKE '%pdf%'
+                 OR itemAttachments.contentType LIKE '%djvu%' )
+"""
+
+_QUERY_DATES = """
+    SELECT parentInfo.itemID,
+           parentItemDataValues.value
+    FROM   itemAttachments
+           INNER JOIN items AS attachmentInfo
+                   ON attachmentInfo.itemID = itemAttachments.itemID
+           INNER JOIN itemData AS attachmentItemData
+                   ON attachmentItemData.itemID = attachmentInfo.itemID
+           INNER JOIN itemDataValues AS attachmentItemDataValues
+                   ON attachmentItemData.valueID =
+                      attachmentItemDataValues.valueID
+           INNER JOIN items AS parentInfo
+                   ON itemAttachments.parentItemID = parentInfo.itemID
+           INNER JOIN itemData AS parentItemData
+                   ON parentItemData.itemID = parentInfo.itemID
+           INNER JOIN itemDataValues AS parentItemDataValues
+                   ON parentItemDataValues.valueID = parentItemData.valueID
+    WHERE parentItemData.fieldID=6
+"""
+
+_QUERY_PATHS = """
+    SELECT parentInfo.itemID,
+           path
+    FROM   itemAttachments
+           INNER JOIN items AS attachmentInfo
+                   ON attachmentInfo.itemID = itemAttachments.itemID
+           INNER JOIN itemData AS attachmentItemData
+                   ON attachmentItemData.itemID = attachmentInfo.itemID
+           INNER JOIN itemDataValues AS attachmentItemDataValues
+                   ON attachmentItemData.valueID =
+                      attachmentItemDataValues.valueID
+           INNER JOIN items AS parentInfo
+                   ON itemAttachments.parentItemID = parentInfo.itemID
+           INNER JOIN itemData AS parentItemData
+                   ON parentItemData.itemID = parentInfo.itemID
+           INNER JOIN itemDataValues AS parentItemDataValues
+                   ON parentItemDataValues.valueID = parentItemData.valueID
+    WHERE  attachmentItemData.fieldID = 1
+           AND parentItemData.fieldID = 1
+           AND ( itemAttachments.contentType LIKE '%pdf%'
+                 OR itemAttachments.contentType LIKE '%djvu%' )
+"""
+
+
+def format_authors(author_list):
+    """Format author string depending on the number of authors.
+
+    Parameters
+    ----------
+    author_list : list
+        A list of all authors.
+
+    Returns
+    -------
+    str
+        String of formatted author list.
+
+    """
+    if len(author_list) == 1:
+        author_string = FORMAT_AUTHORS_SINGLE.format(author1=author_list[0])
+    elif len(author_list) == 2:
+        author_string = FORMAT_AUTHORS_TWO.format(author1=author_list[0],
+                                                  author2=author_list[1])
+    else:
+        author_string = FORMAT_AUTHORS_MULTIPLE.format(author1=author_list[0])
+
+    return author_string
+
+
+def format_item(title, author=None, year=None):
+    """Format item string depending on if author and year information are
+    available.
+
+    Parameters
+    ----------
+    title : str
+        Title of item.
+    author : str
+        Author information for item.
+    year : str
+        Publishing year for item.
+
+    Returns
+    -------
+    str
+        String representation of time.
+
+    """
+    if author and year:
+        item_string = FORMAT_ITEM.format(title=title, author=author, year=year)
+    elif author:
+        item_string = FORMAT_ITEM_WITHOUT_YEAR.format(title=title,
+                                                      author=author)
+    elif year:
+        item_string = FORMAT_ITEM_WITHOUT_AUTHOR.format(title=title, year=year)
+    else:
+        item_string = FORMAT_ITEM_WITHOUT_AUTHOR_AND_YEAR.format(title=title)
+
+    return item_string
+
+
+def get_item_info(zotero_sqlite_file, query):
+    """Get item information from Zotero.
+
+    Parameters
+    ----------
+    zotero_sqlite_file : str
+        Path to Zotero database.
+    query : str
+        SQL query.
+
+    Returns
+    -------
+    list
+        List of items return from the SQL query.
+
+    """
+    conn = sqlite3.connect(zotero_sqlite_file)
+    items = list(conn.execute(query))
+    conn.close()
+    return items
+
+
+def open_file(app, file_path):
+    """Attempt to open ``file_path`` using ``app``.
+
+    Parameters
+    ----------
+    app : str
+        Application to open file. Should contain '%u' for where the file path
+        would appear in the command. If '%u' is not provided, it is assumed
+        that the file path is the last argument to the command.
+    file_path : str
+        Path to file to open.
+
+    """
+    app_and_args = [arg if arg != "%u" else file_path for arg in app.split()]
+    if "%u" not in app:
+        app_and_args.append(file_path)
+    subprocess.run(app_and_args)
+
+
+def show_error(error_msg, rofi_args=None):
+    """Show ``error_msg`` as an error message in Rofi and stderr."""
+    if rofi_args is None:
+        rofi_args = []
+    print(error_msg, file=sys.stderr)
+    subprocess.run(["rofi", "-e", error_msg] + rofi_args)
+
+
+def main():
+    zotero_path = DEFAULT_ZOTERO_PATH
+    base_dir = Path(DEFAULT_ZOTERO_BASE_DIR)
+    rofi_args = DEFAULT_ROFI_EXTRA_FLAGS
+    viewer = DEFAULT_VIEWER
+
+    # Create a temporary copy of the database to avoid issues with locking
+    tmp_dir = Path(tempfile.gettempdir())
+    database_copy = tmp_dir / _ZOTERO_SQLITE_FILE
+    shutil.copy2(zotero_path / _ZOTERO_SQLITE_FILE, database_copy)
+
+    all_authors = get_item_info(database_copy, _QUERY_AUTHORS)
+    all_titles = get_item_info(database_copy, _QUERY_TITLES)
+    all_dates = get_item_info(database_copy, _QUERY_DATES)
+    all_paths = get_item_info(database_copy, _QUERY_PATHS)
+
+    os.remove(database_copy)
+
+    id = all_authors[0][0]
+    author_list = []
+    authors = {}
+    for author_id, author in all_authors:
+        if author_id != id:
+            authors[id] = format_authors(author_list)
+            author_list = []
+            id = author_id
+
+        author_list.append(author)
+
+    titles = {}
+    keys = {}
+    for title_id, title, key in all_titles:
+        if title_id not in titles:
+            titles[title_id] = title
+            keys[title_id] = key
+
+    years = {}
+    for date_id, date in all_dates:
+        if date_id not in years:
+            years[date_id] = date.split("-")[0]
+
+    paths = {}
+    for path_id, path in all_paths:
+        if path is None:
+            continue
+
+        if path_id in paths and str(paths[path_id]) == path:
+            continue
+
+        if path_id not in paths:
+            if path.startswith("attachments:"):
+                path = path.replace("attachments:", "", 1)
+                path = base_dir / path
+            elif path.startswith("storage:"):
+                path = path.replace("storage:", "", 1)
+                path = zotero_path / _ZOTERO_STORAGE_DIR / keys[path_id] / path
+            else:
+                path = Path(path)
+
+            paths[path_id] = path
+
+    item_list_with_ids = []
+    for item_id, title in titles.items():
+        author = authors.get(item_id)
+        year = years.get(item_id)
+        item_string = format_item(title=title, author=author, year=year)
+        item_list_with_ids.append((item_string, item_id))
+
+    item_list_with_ids.sort()
+    item_list = [item for item, _ in item_list_with_ids]
+
+    rofi = subprocess.run(["rofi", "-dmenu", "-format", "i"] + rofi_args,
+                          capture_output=True, text=True,
+                          input="\n".join(item_list))
+    selected_index = rofi.stdout.strip()
+
+    if selected_index:
+        item_id = item_list_with_ids[int(selected_index)][1]
+        file_to_open = paths[item_id]
+        if file_to_open.exists():
+            open_file(viewer, file_to_open)
+        else:
+            show_error(f"Could not find file: {file_to_open}", rofi_args)
+
+
+if __name__ == "__main__":
+    main()
